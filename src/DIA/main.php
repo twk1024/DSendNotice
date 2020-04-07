54241ef38be4ac1c5333f3540fcea7b8f0382ce3
twk1024 <42838268+twk1024@users.noreply.github.com>
2020-04-07 23:34:01 +0900

    Update main.php

--- src/DIA/main.php
+++ src/DIA/main.php
@@ -2,4 +2,24 @@
 
 namespace DIA;
 
-use pocketmine\plugin\
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class main extends PluginBase {
+
+    public function onCommand(CommandSender $sender, Command $cmd, String $Label, array $args): bool{
+        if($cmd->getName()){
+            switch($args(count) === 0){
+              case "notification":
+                $player->sendMessage(C::GREEN . "Emergency - Your server has infected by DOKIDIA");
+                $player->addTitle(C::GREEN . "MY First" . "Plugin");
+                break;
+            }
+        return true;
+        }
+      
+     }
+        
+    }
