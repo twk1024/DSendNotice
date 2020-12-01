9522f4ee47fc4221ba8693b334342dce04d095e4
dokidia <42838268+twk1024@users.noreply.github.com>
2020-12-02 04:29:54 +0900

    Fix Bugs

--- src/DIA/main.php
+++ src/DIA/main.php
@@ -6,20 +6,16 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
 
 class main extends PluginBase {
 
     public function onCommand(CommandSender $sender, Command $cmd, String $Label, array $args): bool{
-        if($cmd->getName()){
-            switch($args(count) === 0){
-              case "notification":
-                $player->sendMessage(C::GREEN . "Emergency - Your server has infected by DOKIDIA");
-                $player->addTitle(C::GREEN . "MY First" . "Plugin");
-                break;
+        if($cmd->getName("notification")){
+                $player->sendMessage(TextFormat::GREEN . "DOKIDIA");
+                $player->addTitle(TextFormat::GREEN . "MY First" . "Plugin");
             }
         return true;
         }
       
      }
-        
-    }
